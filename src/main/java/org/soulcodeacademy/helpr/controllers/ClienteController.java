92948ea4ab50d87c072a99c6723f0e40b4fd5c0a
aeromandy <aeromandy2016@gmail.com>
2022-12-07 13:52:43 -0300

    correcao anotation controllers

--- src/main/java/org/soulcodeacademy/helpr/controllers/ClienteController.java
+++ src/main/java/org/soulcodeacademy/helpr/controllers/ClienteController.java
@@ -15,32 +15,32 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_FUNCIONARIO')")
     @GetMapping("/clientes")
     public List<Cliente> mostrarLista() {
         return this.clienteService.listarTodos();
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_FUNCIONARIO')")
     @GetMapping("/clientes/{idCliente}")
     public Cliente getCliente(@PathVariable Integer idCliente){
         return this.clienteService.getCliente(idCliente);
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_FUNCIONARIO')")
     @PostMapping("/clientes")
     public Cliente salvarCliente(@Valid @RequestBody ClienteDTO dto) {
         Cliente cliente = this.clienteService.salvar(dto);
         return cliente;
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_FUNCIONARIO')")
     @PutMapping("/clientes/{idCliente}")
     public Cliente atualizar(@PathVariable Integer idCliente, @Valid @RequestBody ClienteDTO dto) {
         Cliente atualizado = this.clienteService.atualizar(idCliente, dto);
         return atualizado; // CORPO da Resposta em JSON
     }
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_FUNCIONARIO')")
     @DeleteMapping("/clientes/{idCliente}")
     public void deletar(@PathVariable Integer idCliente) {
         this.clienteService.deletar(idCliente);
