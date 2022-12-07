a8e8f558da09ff422b4c6478767692869ad2ce47
aeromandy <aeromandy2016@gmail.com>
2022-12-07 10:47:52 -0300

    protecao endpoints funcionario

--- src/main/java/org/soulcodeacademy/helpr/controllers/FuncionarioController.java
+++ src/main/java/org/soulcodeacademy/helpr/controllers/FuncionarioController.java
@@ -4,6 +4,7 @@ import org.soulcodeacademy.helpr.domain.Funcionario;
 import org.soulcodeacademy.helpr.domain.dto.FuncionarioDTO;
 import org.soulcodeacademy.helpr.services.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,38 +15,40 @@ public class FuncionarioController {
     @Autowired
     private FuncionarioService funcionarioService;
 
-    // /funcionarios (GET)
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
     @GetMapping("/funcionarios")
     public List<Funcionario> listar() {
         return this.funcionarioService.listar();
     }
 
     // @RequestParam = Captura os valores de parâmetro após ?, ex: /funcionarios/salario?valor1=1000&valor2=2000
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
     @GetMapping("/funcionarios/salario")
     public List<Funcionario> listarPorFaixaSalarial(@RequestParam Double valor1, @RequestParam Double valor2) {
         return this.funcionarioService.listarPorFaixaSalarial(valor1, valor2);
     }
 
     // /funcionarios/{id} (GET)
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
     @GetMapping("/funcionarios/{idFuncionario}")
     public Funcionario getFuncionario(@PathVariable Integer idFuncionario) {
         return this.funcionarioService.getFuncionario(idFuncionario);
     }
 
-    // POST = Representa inserção de dados
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_FUNCIONARIO')")
     @PostMapping("/funcionarios")
     public Funcionario salvar(@Valid @RequestBody FuncionarioDTO dto) {
         Funcionario funcionario = this.funcionarioService.salvar(dto);
         return funcionario;
     }
 
-    // PUT = Representa substituição de dados
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PutMapping("/funcionarios/{idFuncionario}")
     public Funcionario atualizar(@PathVariable Integer idFuncionario, @Valid @RequestBody FuncionarioDTO dto) {
         Funcionario atualizado = this.funcionarioService.atualizar(idFuncionario, dto);
         return atualizado;
     }
-
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @DeleteMapping("/funcionarios/{idFuncionario}")
     public void deletar(@PathVariable Integer idFuncionario) {
         this.funcionarioService.deletar(idFuncionario);
